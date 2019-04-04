faa1023c85ffffa2d9eee1613497a60929672cac
rupalkataria <34120751+rupalkataria@users.noreply.github.com>
2019-04-04 14:12:14 +0530

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,1 +1,8 @@
 
+var arr = [1, 2, 3];
+if (x < 0) {
+    arr[x] = 3; // ARRAY_INDEX_NEGATIVE alarm: x is negative but is used as array index.
+}
+if (x <= 0) {
+    if (x) arr[x] = 3; // ARRAY_INDEX_NEGATIVE alarm: x is negative but is used as array index.
+}
